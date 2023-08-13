2df724f9ad7045a17bc527054e2b2694bd34f7b8
AyushiChopra02 <139689074+AyushiChopra02@users.noreply.github.com>
2023-08-13 23:07:35 +0530

    add

--- SimpleC/areaofCircleInput.c
+++ SimpleC/areaofCircleInput.c
@@ -4,7 +4,7 @@ float radius;
 printf("Enter radius: " );
 scanf("%f",&radius);
 float pi= 3.14;
-float area= pi* radius * radius;
+float area = pi* radius * radius;
 printf("the are of circle is :%f", area);
 return 0;
 
